a21577395375b7bb07f41c3bfce7e194b677f6f3
Velkan00 <velkan00@gmail.com>
2023-04-05 21:22:35 -0400

    eua

--- src/999_Diario_React/DummyJSONLoggin/Components/Loader.tsx
+++ src/999_Diario_React/DummyJSONLoggin/Components/Loader.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-// import '../../../styles/loader.css';
 
 export default function Loader() {
  return (
-  <div className=" inline-block w-[50px] h-[50px] after:content-['_'] after:block after:w-[50px] after:h-[50px] after:m-[8px] after:rounded-[50%] after:[border:6px_solid_#5c36f8] after:[border-color:#5c36f8_transparent_#5c36f8_transparent] after:animate-spin" />
+  <div className=" inline-block w-[50px] h-[50px] after:content-['_'] after:block after:w-[50px] after:h-[50px] after:m-[8px] after:rounded-[50%] after:[border:6px_solid_#5c36f8] after:[border-color:_#5c36f8_transparent_#5c36f8_transparent] after:animate-spin" />
  );
 }
