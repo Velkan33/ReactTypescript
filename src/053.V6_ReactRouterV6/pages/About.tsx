a7ac34e2610099f3758adebb8f79852869aa4a08
Velkan00 <velkan00@gmail.com>
2023-06-11 12:47:08 -0400

    comu

--- src/053.V6_ReactRouterV6/pages/About.tsx
+++ src/053.V6_ReactRouterV6/pages/About.tsx
@@ -1,9 +1,22 @@
+import React from 'react';
 import { useMyState } from '../context/ContextProvider';
 
 export default function About() {
  const statePack = useMyState();
+
  if (statePack === null) return <h2>Error en el state</h2>;
  const { state, setState } = statePack;
- setState('Kevin');
- return <h1>About {state}</h1>;
+
+ return (
+  <>
+   <h1>About {state}</h1>
+   <button
+    className="bg-slate-200 px-2 py-1 m-1 rounded"
+    type="button"
+    onClick={() => setState('Kevin')}
+   >
+    click to change
+   </button>
+  </>
+ );
 }
---
+++ src/053.V6_ReactRouterV6/pages/ServiceDisplay.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function ServiceDisplay({
+ services,
+}: {
+ services: { id: number; name: string; price: string }[];
+}) {
+ const actualServiceId = useParams();
+
+ const actualService = services.find(
+  (param) => actualServiceId.id === param.id.toString()
+ );
+ if (actualService === undefined) return <p>Service Id undefined</p>;
+ return (
+  <>
+   <p>service: {actualService.name}</p>
+   <p>price: {actualService.price}</p>
+  </>
+ );
+}
