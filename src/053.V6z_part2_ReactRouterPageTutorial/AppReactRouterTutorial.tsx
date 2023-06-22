d943296ee009f30912a9cf06420119e2fe78e4d4
VelkanAlpha <velkan00@gmail.com>
2023-06-22 15:17:36 -0400

    ko

--- src/053.V6z_part2_ReactRouterPageTutorial/AppReactRouterTutorial.tsx
+++ src/053.V6z_part2_ReactRouterPageTutorial/AppReactRouterTutorial.tsx
@@ -5,19 +5,19 @@ import {
  createBrowserRouter,
  // Create a Component with a param router that will have the created browser router.
  RouterProvider,
- // Return an object inside the 'errorElement' component, that is called, this error object can be used to expose the error through statusText or message key.
+ // Return an object inside the 'errorElement' component, that is called, this error object can be used to expose the error through statusText or message key. It is used in the 'error-page' file
  useRouteError,
- // This will access the children component to use it inside the parent component
+ // This will access the children component to use it inside the parent component.It is used in the 'root' file
  Outlet,
  // This will change the route address
  Link,
- // This will load the loader data to be used in the component
+ // This will load the loader data to be used in the component.It is used on several components
  useLoaderData,
- // This seems like a regular form but will have some extra uses like 'action', that will change the URL and like 'method'
+ // This seems like a regular form but will have some extra uses like 'action', that will change the URL and like 'method'.It is used on several components.`
  Form,
  // This callback will redirect to a parameter URL (In this app is used as a return on the 'edit/EditContact' component)
  redirect,
- // This is the type of the action parameters
+ // This is the type of the action parameters, is to use in typescript.It is called on severals components
  ActionFunctionArgs,
  // This is similar to Link except that it have parameters isActive and isPending, that can be used to change the className concurrently.
  NavLink,
@@ -37,12 +37,13 @@ import {
  //     "formAction": "/contacts/ygcjrgl/destroy",
  //     "formEncType": "application/x-www-form-urlencoded",
  //     "formData": {}
- // }
+ // }.Is used in the 'Root' component
  useNavigation,
- // Is used to return or advance an amount of pages
+ // Is used to return or advance an amount of pages.It is used in 'edit' file.
  useNavigate,
- // Is used to serialize and submit the element pass to it, in this project is used to send a get form for the search bar
+ // Is used to serialize and submit the element pass to it, in this project is used to send a get form for the search bar in the 'root' file.
  useSubmit,
+ // Is to send a form without changing the url, is used in the 'Contact' component.
  useFetcher,
 } from 'react-router-dom';
 
